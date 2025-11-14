2b14457970267f99c5aef7a331c497d4d1468d08
veliok <veli.okkonen@gmail.com>
2025-11-14 18:00:52 +0200

    Group management

---
+++ api/src/models/group_model.js
@@ -0,0 +1,116 @@
+/*
+  This file holds:
+  - Group creation and deletion
+  - Group member management
+  - Group request management
+*/
+
+import pool from "../database.js"
+
+// GROUP
+export async function createGroup(userId, groupName, description) {
+  const result = await pool.query(`
+    INSERT INTO groups (group_name, description, created_at, owner_id) 
+    VALUES ($1, $2, NOW(), $3)
+    RETURNING group_id`,
+    [groupName, description, userId]
+  );
+
+  const groupId = result.rows[0].group_id;
+  await pool.query(`
+    INSERT INTO group_members (group_id, user_id, role)
+    VALUES ($1, $2, 'owner')`,
+    [groupId, userId]
+  );
+
+  return result.rows[0];
+}
+
+export async function getGroups() {
+  const result = await pool.query(`SELECT * FROM groups`);
+  return result.rows;
+}
+
+export async function getGroup(groupId) {
+  const result = await pool.query(`
+    SELECT * FROM groups 
+    WHERE group_id = $1`, [groupId]);
+  return result.rows[0];
+}
+
+export async function deleteGroup(groupId) {
+  const result = await pool.query(`DELETE FROM groups WHERE group_id = $1`, [groupId]);
+  await pool.query('DELETE FROM group_members WHERE group_id = $1', [groupId]);
+  return result.rows;
+}
+
+// GROUP MEMBER
+export async function getGroupMembers(groupId) {
+  const result = await pool.query(`
+    SELECT user_id, role
+    FROM group_members
+    WHERE group_id = $1
+    AND (role = 'owner' OR role = 'member')`, 
+    [groupId]
+  );
+
+  return result.rows;
+}
+
+export async function isOwner(groupId, userId) {
+  const result = await pool.query(`
+    SELECT user_id FROM group_members
+    WHERE role = 'owner' AND group_id = $1 AND user_id = $2`, [groupId, userId]);
+  return result.rows;
+}
+
+export async function isMember(groupId, userId) {
+  const result = await pool.query(`
+    SELECT user_id FROM group_members
+    WHERE role = 'member'
+    AND group_id = $1 AND user_id = $2`, [groupId, userId]);
+  return result.rows;
+}
+
+export async function removeMember(groupId, userId) {
+  const result = await pool.query(`
+    DELETE FROM group_members
+    WHERE group_id = $1 AND user_id = $2
+    RETURNING *`, [groupId, userId]);
+  return result.rows[0];
+}
+
+// REQUESTS
+export async function createJoinRequest(groupId, userId) {
+  const result = await pool.query(`
+    INSERT INTO group_members (group_id, user_id, role)
+    VALUES ($1, $2, 'requested') RETURNING *`,
+    [groupId, userId]
+  );
+
+  return result.rows[0];
+}
+
+export async function getJoinRequests(groupId) {
+  const result = await pool.query(`
+    SELECT * FROM group_members WHERE group_id = $1
+    AND role = 'requested'`, [groupId]);
+  return result.rows;
+}
+
+export async function acceptJoinRequest(groupId, userId) {
+  const result = await pool.query(`
+    UPDATE group_members 
+    SET role = 'member' 
+    WHERE group_id = $1 AND user_id = $2 AND role = 'requested'
+    RETURNING *`, [groupId, userId]);
+  return result.rows[0];
+}
+
+export async function rejectJoinRequest(groupId, userId) {
+  const result = await pool.query(`
+    DELETE FROM group_members
+    WHERE group_id = $1 AND user_id = $2
+    RETURNING *`, [groupId, userId]);
+  return result.rows[0] || null;
+}
